e0bbd875e204f2bb1f897bd21f33908d363fd6c0
Carl-Emil Kjellstrand <erbsman@gmail.com>
2013-09-22 18:42:01 +0200

    completed 187, starting 237.

---
+++ com/kjellstrand/euler/Euler0237.java
@@ -0,0 +1,27 @@
+package com.kjellstrand.euler;
+
+
+/*
+    Let T(n) be the number of tours over a 4 × n playing board such that:
+
+    The tour starts in the top left corner.
+    The tour consists of moves that are up, down, left, or right one square.
+    The tour visits each square exactly once.
+    The tour ends in the bottom left corner.
+    The diagram shows one tour over a 4 × 10 board:
+
+    T(10) is 2329. What is T(1012) modulo 108?
+ */
+
+public class Euler0237 {
+
+	public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+
+        // look only at row 2, all depends on either of the middle rows.
+        
+        System.out.println("time: " + (System.currentTimeMillis() - time) + "ms"); 
+
+	}
+}
+
---
+++ completed/Euler0187.java
@@ -0,0 +1,56 @@
+package completed;
+
+import com.kjellstrand.euler.Tools;
+
+// A composite is a number containing at least two prime factors.
+// For example, 15 = 3 × 5; 9 = 3 × 3; 12 = 2 × 2 × 3.
+//
+// There are ten composites below thirty containing precisely two,
+// not necessarily distinct, prime factors:
+// 4, 6, 9, 10, 14, 15, 21, 22, 25, 26.
+//
+// How many composite integers, n < 10^8, have precisely two,
+// not necessarily distinct, prime factors?
+
+public class Euler0187 {
+
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+
+        Long problemSize = 100000000l;
+        // 17 427 258
+        Long maxPrime = problemSize / 2;
+        Integer[] iprimes = Tools.getPrimes(maxPrime.intValue()).toArray(new Integer[0]);
+        Long primes[] = new Long[iprimes.length];
+        for (int i = 0; i < iprimes.length; i++) {
+            primes[i] = new Long(iprimes[i]);
+        }
+        long sum = 1;
+        for (int i = 0; i < Math.sqrt(primes.length); i++) {
+
+            Long p = bs(new Long(0), new Long(primes.length - 1),
+                    new Long(primes.length / 2), primes, problemSize, primes[i]);
+
+            if(p>i){
+            sum += p-i;
+            }
+            
+        }
+        System.out.println(sum);
+
+        System.out.println("time: " + (System.currentTimeMillis() - time) + "ms");
+    }
+
+    private static Long bs(Long lb, Long ub, Long p, Long[] primes, Long problemSize, Long prime) {
+        if (ub - lb<=1) {
+            return ub;
+        }
+        if (prime * primes[p.intValue()] < problemSize) {
+            lb = p;
+        } else {
+            ub = p;
+        }
+        p = (ub - lb ) / 2 + lb;
+        return bs(lb, ub, p, primes, problemSize, prime);
+    }
+}
