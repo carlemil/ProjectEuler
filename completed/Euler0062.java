4f38ff0763dbaa937d2d12f491f5a51c9dc8caf1
Carl-Emil Kjellstrand <erbsman@gmail.com>
2013-11-17 22:10:14 +0100

    solved 62.

---
+++ completed/Euler0062.java
@@ -0,0 +1,52 @@
+package completed;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+// The cube, 41063625 (345^3), can be permuted to produce two other
+// cubes: 56623104 (384^3) and 66430125 (405^3). In fact, 41063625 is
+// the smallest cube which has exactly three permutations of its
+// digits which are also cube.
+//
+// Find the smallest cube for which exactly five permutations of its
+// digits are cube.
+
+public class Euler0062 {
+
+    static HashMap<String, Pair<Long, Integer>> map = new HashMap<String, Pair<Long, Integer>>();
+
+    public static void main(String[] args) {
+        long time = System.currentTimeMillis();
+
+        boolean search = true;
+        int q = 1;
+        Long q3 = new Long(0);
+        while (search) {
+            q3 = (long) Math.pow(q++, 3);
+            char[] q3ca = q3.toString().toCharArray();
+            java.util.Arrays.sort(q3ca);
+            String s = Arrays.toString(q3ca);
+            if(map.containsKey(s)){
+                Pair<Long, Integer> pair = map.get(s);
+                if (++pair.count == 5) {
+                    System.out.println(pair.number);
+                    search = false;
+                }
+            } else {
+                map.put(s, new Pair<Long, Integer>(q3,1));
+            }
+        }
+
+        System.out.println("time: " + (System.currentTimeMillis() - time) + "ms");
+    }
+}
+
+class Pair<T1, T2> {
+    public Long number = 0l;
+    public Integer count = 0;
+    
+    public Pair(T1 number, T2 count) {
+        this.number = (Long) number;
+        this.count = (Integer) count;
+    }
+}
